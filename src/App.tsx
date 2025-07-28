7220cbd266c1e1164b46e02adaee0d3ef427abe3
Tomás Frazão <tomas.santos.frazao@gmail.com>
2025-07-28 12:06:29 +0100

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,27 @@
 import { BrowserRouter } from "react-router-dom";
 import { Router } from "./router";
 import "./styles/global.css";
+import express from "express";
 
 function App() {
+
+  const app = express();
+  app.use(express.static("src/assets"));
+  app.get("/imagem", (req, res) => {
+    
+    const password = req.query.password;
+    const imageName = req.query.name;
+
+    if (password === process.env.PASS) {
+      res.sendFile(`${__dirname}/assets/${imageName}`);
+    } else {
+      res.status(403).send("Forbidden");
+    }
+  });
+
+  app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+  });
 
   return (
     <BrowserRouter>
